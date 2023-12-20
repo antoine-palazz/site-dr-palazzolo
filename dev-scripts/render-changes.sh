968fa36f3f41e08fec37e8c4128c3f7beed49298
Quarto GHA Workflow Runner <quarto-github-actions-publish@example.com>
2023-12-20 13:30:10 +0000

    Built site for gh-pages

--- dev-scripts/render-changes.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-git diff --name-only >> diff
-python build/tweak_render.py
-
-quarto render
-find . -name "index.qmd" | rename -f -d 's/^/_/'
-
-quarto preview --port 5000 --host 0.0.0.0
--- dev-scripts/render-chapter.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-SECTION=$1
-CHAPTER=$2
-
-SECTION_PATH="content/${SECTION}"
-CHAPTER_PATH="${SECTION_PATH}/${CHAPTER}"
-
-quarto preview -port 5000 --host 0.0.0.0
